9498ff6b18447b4a29672fde6d8a966af2829b1f
nutella-ops <52546474+nutella-ops@users.noreply.github.com>
2019-10-23 17:39:23 -0700

    Add files via upload

--- create_notes_drs.relative.py
+++ create_notes_drs.relative.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
 import os
-cwd = os.getcwd()
+current_dir = os.getcwd()
 
 #function to make 24 week_dir
-define sub_week():
-    for enum in range(1,25):
-        os.mkdir(week_+"enum")
+#define sub_week():
+    #for enum in range(1,25):
+        #os.mkdir(week_+"enum")
 
 #function to make 3 day_dir
-define sub_day():
-    for enum in range(1,4):
-        os.mkdir(day_+"enum")
+#define sub_day():
+    #for enum in range(1,4):
+        #os.mkdir(day_+"enum")
 
-#define master_dir
-root = ("CyberSecurity-Notes")
+#name the filesystem
+home_name = ("CyberSecurity-Notes")
 
 #function call to create sub_dirs
-notes_folder = os.path.join(cwd, root, "week_","day_")
-os.makedirs(notes_folder)
+filesystem = os.path.join(current_dir, home_name, "week_","day_")
+os.makedirs(filesystem)
 
 #try:
     #os.mkdir(root)
