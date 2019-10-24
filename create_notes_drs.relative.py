fe0bddd5da7a51d78eda3219f22618b8b58cadb6
nutella-ops <52546474+nutella-ops@users.noreply.github.com>
2019-10-23 17:31:47 -0700

    Add files via upload
    week_dir & day_dir because didnt know how else to do it

--- create_notes_drs.relative.py
+++ create_notes_drs.relative.py
@@ -2,12 +2,22 @@
 
 import os
 cwd = os.getcwd()
-print(cwd)
 
+#function to make 24 week_dir
+define sub_week():
+    for enum in range(1,25):
+        os.mkdir(week_+"enum")
+
+#function to make 3 day_dir
+define sub_day():
+    for enum in range(1,4):
+        os.mkdir(day_+"enum")
+
+#define master_dir
 root = ("CyberSecurity-Notes")
 
+#function call to create sub_dirs
 notes_folder = os.path.join(cwd, root, "week_","day_")
-
 os.makedirs(notes_folder)
 
 #try:
