a1984c932855de38e7f3059cccfc1935af409b6a
nutella-ops <52546474+nutella-ops@users.noreply.github.com>
2019-10-23 19:34:36 -0700

    Add files via upload
    just working thru errors and making filesystem behave properly

--- create_notes_drs.relative.py
+++ create_notes_drs.relative.py
@@ -4,26 +4,25 @@ import os
 current_dir = os.getcwd()
 
 #function to make 24 week_dir
-#define sub_week():
-    #for enum in range(1,25):
-        #os.mkdir(week_+"enum")
-
+def sub_week():
+    for enum in range(1,25):
+        week_name = os.mkdir("week_" + str(enum))
+        return week_name
 #function to make 3 day_dir
-#define sub_day():
-    #for enum in range(1,4):
-        #os.mkdir(day_+"enum")
+def sub_day():
+    for enum in range(1,4):
+        day_name = os.mkdir("day_" + str(enum))
+        return day_name
+try:
+    #name the filesystem
+    home_folder = ("CyberSecurity-Notes")
+    week = sub_week()
+    day = sub_day()
 
-#name the filesystem
-home_name = ("CyberSecurity-Notes")
+    #function call to create sub_dirs
+    filesystem = os.path.join(current_dir, home_folder, week, day)
+    os.makedirs(filesystem)
 
-#function call to create sub_dirs
-filesystem = os.path.join(current_dir, home_name, "week_","day_")
-os.makedirs(filesystem)
-
-#try:
-    #os.mkdir(root)
-    #for num in range(1,25):
-        #os.mkdir(sub_week + str(num))
-#except:
-    ##print("Directory:" + str(root) + " already exists, dummy.")
+except:
+    print("Directory:" + str(home_folder) + " already exists, dummy.")
   
